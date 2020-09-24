df2db50efa667d926bc50c0aca928b982cb867b0
zubairdange <71386093+zubairdange@users.noreply.github.com>
2020-09-24 20:41:59 +0530

    email ID updated

--- src/MyForm.jsx
+++ src/MyForm.jsx
@@ -5,7 +5,7 @@ export const MyForm = () => {
     <Form>
       <Form.Group controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter your email" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
